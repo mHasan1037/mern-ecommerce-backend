00331856a168b6999b1c81c7a63e6e4ebb0c7f73
Mahmudul Hasan <mhasan.bd.95@gmail.com>
2025-05-06 20:26:09 +0600

    fixed the cookies

--- utils/setTokenCookies.js
+++ utils/setTokenCookies.js
@@ -7,26 +7,27 @@ const setTokenCookies = (
 ) => {
     const accessTokenMaxAge = (newAccessTokenExp - Math.floor(Date.now() / 1000)) * 1000;
     const refreshTokenMaxAge = (newRefreshTokenExp - Math.floor(Date.now() / 1000)) * 1000;
+    const isProd = process.env.NODE_ENV === "production";
 
     res.cookie('accessToken', accessToken, {
         httpOnly: true,
         secure: process.env.NODE_ENV === "production",
         maxAge: accessTokenMaxAge,
-        sameSite: 'lax',
+        sameSite: isProd ? "none" : "lax",
     })
 
     res.cookie('refreshToken', refreshToken, {
         httpOnly: true,
         secure: process.env.NODE_ENV === "production",
         maxAge: refreshTokenMaxAge,
-        sameSite: 'lax'
+        sameSite: isProd ? "none" : "lax",
     })
 
     res.cookie('accessToken_public', accessToken, {
         httpOnly: false,
         secure: process.env.NODE_ENV === "production",
         maxAge: accessTokenMaxAge,
-        sameSite: "lax",
+        sameSite: isProd ? "none" : "lax",
     })
 }
 
