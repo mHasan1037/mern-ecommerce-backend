c0906610ceb3844ad30f38ac6a3f4c53cc07165e
Mahmudul Hasan <mhasan.bd.95@gmail.com>
2025-11-23 16:03:19 +0600

    Finised the integration of sslcommerze

--- app.js
+++ app.js
@@ -1,28 +1,29 @@
-import dotenv from 'dotenv';
+import dotenv from "dotenv";
 dotenv.config();
-import express from 'express';
-import cors from 'cors';
-import cookieParser from 'cookie-parser';
-import connectDB from './config/connectdb.js';
-import userRoutes from './routes/userRoutes.js';
-import productRoutes from './routes/productRoutes.js';
-import cartRoutes from './routes/cartRoutes.js';
-import orderRoutes from './routes/orderRoutes.js';
-import wishlistRoutes from './routes/wishlistRoutes.js';
-import adminRoutes from './routes/adminRoutes.js';
-import chatbotRoutes from './routes/chatBotRoutes.js';
-import passport from 'passport';
-import './config/passport-jwt-strategy.js';
+import express from "express";
+import cors from "cors";
+import cookieParser from "cookie-parser";
+import connectDB from "./config/connectdb.js";
+import userRoutes from "./routes/userRoutes.js";
+import productRoutes from "./routes/productRoutes.js";
+import cartRoutes from "./routes/cartRoutes.js";
+import orderRoutes from "./routes/orderRoutes.js";
+import wishlistRoutes from "./routes/wishlistRoutes.js";
+import adminRoutes from "./routes/adminRoutes.js";
+import chatbotRoutes from "./routes/chatBotRoutes.js";
+import sslRoutes from "./payment/sslRoutes.js";
+import passport from "passport";
+import "./config/passport-jwt-strategy.js";
 
 const app = express();
 const port = process.env.PORT;
-const DATABASE_URL = process.env.DATABASE_URL
+const DATABASE_URL = process.env.DATABASE_URL;
 
 const corsOptions = {
-    origin: process.env.FRONTEND_HOST,
-    credentials: true,
-    optionSuccessStatus: 200,
-  };
+  origin: process.env.FRONTEND_HOST,
+  credentials: true,
+  optionSuccessStatus: 200,
+};
 
 app.use(cors(corsOptions));
 connectDB(DATABASE_URL);
@@ -30,16 +31,15 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(passport.initialize());
 app.use(cookieParser());
-app.use('/api/user', userRoutes);
-app.use('/api', productRoutes);
-app.use('/api', cartRoutes);
-app.use('/api', orderRoutes);
-app.use('/api', wishlistRoutes);
-app.use('/api', adminRoutes);
-app.use('/api', chatbotRoutes);
+app.use("/api/payment/ssl", sslRoutes);
+app.use("/api/user", userRoutes);
+app.use("/api", productRoutes);
+app.use("/api", cartRoutes);
+app.use("/api", orderRoutes);
+app.use("/api", wishlistRoutes);
+app.use("/api", adminRoutes);
+app.use("/api", chatbotRoutes);
 
-
-app.listen(port, '0.0.0.0', ()=>{
-    console.log(`Server is listing at http://0.0.0.0:${port}`)
-})
- 
+app.listen(port, "0.0.0.0", () => {
+  console.log(`Server is listing at http://0.0.0.0:${port}`);
+});
