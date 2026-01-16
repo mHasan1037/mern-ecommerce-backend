23c4c1b981158952ef26fff4e3c809a509eb34f2
Mahmudul Hasan <mhasan.bd.95@gmail.com>
2026-01-16 15:27:24 +0600

    Added cache control

--- controllers/orderController.js
+++ controllers/orderController.js
@@ -120,8 +120,8 @@ export const getUserOrders = async (req, res) =>{
 
 export const getAllOrders = async (req, res) =>{
     try{
-      const page = Number(req.query.page) || 1;
-      const limit = Number(req.query.limit) || 10;
+      const page = Math.max(Number(req.query.page) || 1, 1);
+      const limit = Math.max(Number(req.query.limit) || 10, 1);
       const skip = (page - 1) * limit;
       
       const [orders, totalOrders] = await Promise.all([
@@ -134,6 +134,8 @@ export const getAllOrders = async (req, res) =>{
 
         OrderModel.countDocuments()
       ])
+
+      res.set("Cache-Control", "no-store");
 
       res.status(200).json({
         message: "Orders fetched successfully",
