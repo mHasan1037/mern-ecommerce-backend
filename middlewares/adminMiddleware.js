67ed76397fb505f14cac5305ad9f521b6882cc1e
Mahmudul Hasan <mhasan.bd.95@gmail.com>
2026-09-01 15:32:46 +0600

    Fixed reassignable variable

--- middlewares/adminMiddleware.js
+++ middlewares/adminMiddleware.js
@@ -2,7 +2,7 @@ import jwt from "jsonwebtoken";
 
 const adminMiddleware = (req, res, next) => {
   try {
-    const token = req.cookies.accessToken;
+    let token = req.cookies.accessToken;
 
     if (!token && req.headers.authorization?.startsWith('Bearer ')) {
       token = req.headers.authorization.split(' ')[1];
